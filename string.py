172d1bf86727cc19d34c9814fabff26cf1e25674
alvinokullo560-oss <alvinokullo560@gmail.com>
2026-04-28 10:09:46 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+str= "Hello World"
+print(str)
+print(str[4])
+print(str[1:4])
+print(str*3)
+print(str + "next")
+print(str[4:0])
+print(str[0:9])
+
+s ="Learning python is fun!"
+print("length of the string is:",len(s))
+print(s.lower())
+print(s.upper())
+print(s.capitalize())
+print(s.swapcase())
+print(s.title())
+print("Maximum character is:",max(s))
+print("Minimum char is:",min(s))
