81c7311973675c5a7436ce049007a65acce0fca3
alvinokullo560-oss <alvinokullo560@gmail.com>
2026-04-28 09:01:39 +0530

    Add files via upload

---
+++ arithmeticoperator.py
@@ -0,0 +1,7 @@
+ `a , b , c = 4 , 2 , 2
+print("multiplication:", (a*b))
+print("subtraction:", (a-b))
+print("division:", (a/b))
+print("Exponent:", (a**b))
+print("modulas:", (a%b))
+print("floor division:", (a//b))
